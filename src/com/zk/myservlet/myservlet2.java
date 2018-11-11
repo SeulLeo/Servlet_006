9e1d05aa9f004ea6dc318c07fc6884516c94e377
SeulLeo <359269746@qq.com>
2018-11-11 19:46:42 +0800

    Initial commit

---
+++ src/com/zk/myservlet/myservlet2.java
@@ -0,0 +1,52 @@
+package com.zk.myservlet;
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class myservlet2 extends HttpServlet {
+
+	/**
+	 * The doGet method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to get.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("我要办事");
+		System.out.println("你的事我办不了");
+		System.out.println("你的事我找人帮你办");
+		ServletContext application=this.getServletContext();
+		//将请求向下传递
+		application.getRequestDispatcher("/demo3").forward(request, response);
+		
+		System.out.println("事办完了");
+	}
+
+	/**
+	 * The doPost method of the servlet. <br>
+	 *
+	 * This method is called when a form has its tag value method equals to post.
+	 * 
+	 * @param request the request send by the client to the server
+	 * @param response the response send by the server to the client
+	 * @throws ServletException if an error occurred
+	 * @throws IOException if an error occurred
+	 */
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request,response);
+	}
+
+}
